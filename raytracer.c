16179e17a40abc7f8abd80bfa098a6dd4fffa044
Triketek <100535116+Triketek@users.noreply.github.com>
2025-03-28 15:33:53 -0300

    Fix Shadow
    Offset shadow ray to prevent self-intersection artifacts

--- raytracer.c
+++ raytracer.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-// Constants
-#define WIDTH 800
-#define HEIGHT 600
-#define MAX_DEPTH 5
-#define EPSILON 1e-4
-
-// Vector structure and operations
+// Structures
 typedef struct {
     double x, y, z;
 } Vector;
 
-// Function prototypes for vector operations
+typedef struct {
+    Vector origin, direction;
+} Ray;
+
+typedef struct {
+    Vector center, color;
+    double radius;
+} Sphere;
+
+typedef struct {
+    Vector position, normal, color;
+} Plane;
+
+typedef struct {
+    Vector position, color;
+} Light;
+
+// Constants
+#define MAX_DEPTH 3
+#define EPSILON 1e-4
+
+// Function declarations
 Vector vec_add(Vector a, Vector b);
 Vector vec_sub(Vector a, Vector b);
 Vector vec_mul(Vector a, double scalar);
-Vector vec_div(Vector a, double scalar);
-double vec_dot(Vector a, Vector b);
-Vector vec_normalize(Vector a);
-Vector vec_reflect(Vector v, Vector n);
 Vector vec_hadamard(Vector a, Vector b);
+Vector vec_normalize(Vector a);
+double vec_dot(Vector a, Vector b);
+double vec_length(Vector a);
+int intersect_sphere(Ray ray, Sphere sphere, double *t);
+int intersect_plane(Ray ray, Plane plane, double *t);
+int is_in_shadow(Vector point, Vector light_dir);
+Vector trace_ray(Ray ray, int depth);
 
-// Vector operations implementation
+// Global variables
+Sphere spheres[] = {
+    {{0, -0.5, 3}, {1, 0, 0}, 0.5},
+    {{-1, -0.5, 4}, {0, 1, 0}, 0.5},
+    {{1, -0.5, 4}, {0, 0, 1}, 0.5}
+};
+
+Plane plane = {{0, -1, 0}, {0, 1, 0}, {0.3, 0.3, 0.3}}; // Floor at y = -1, facing upwards
+
+Light light = {{2, 5, 0}, {1, 1, 1}};
+
+// Vector operations
 Vector vec_add(Vector a, Vector b) {
     return (Vector){a.x + b.x, a.y + b.y, a.z + b.z};
 }
@@ -36,63 +64,24 @@ Vector vec_mul(Vector a, double scalar) {
     return (Vector){a.x * scalar, a.y * scalar, a.z * scalar};
 }
 
-Vector vec_div(Vector a, double scalar) {
-    return (Vector){a.x / scalar, a.y / scalar, a.z / scalar};
+Vector vec_hadamard(Vector a, Vector b) {
+    return (Vector){a.x * b.x, a.y * b.y, a.z * b.z};
+}
+
+Vector vec_normalize(Vector a) {
+    double length = sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    return vec_mul(a, 1.0 / length);
 }
 
 double vec_dot(Vector a, Vector b) {
     return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
-Vector vec_normalize(Vector a) {
-    double len = sqrt(vec_dot(a, a));
-    return vec_div(a, len);
+double vec_length(Vector a) {
+    return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
 }
 
-Vector vec_reflect(Vector v, Vector n) {
-    return vec_sub(v, vec_mul(n, 2 * vec_dot(v, n)));
-}
-
-// Hadamard product (component-wise multiplication)
-Vector vec_hadamard(Vector a, Vector b) {
-    return (Vector){a.x * b.x, a.y * b.y, a.z * b.z};
-}
-
-// Sphere structure
-typedef struct {
-    Vector center;
-    double radius;
-    Vector color;
-    double reflectivity;
-} Sphere;
-
-// Light structure
-typedef struct {
-    Vector position;
-    Vector color;
-} Light;
-
-// Ray structure
-typedef struct {
-    Vector origin;
-    Vector direction;
-} Ray;
-
-// Scene objects
-Sphere spheres[] = {
-    {{0, 0, -20}, 4, {1, 0, 0}, 0.5},  // Red sphere
-    {{5, -1, -15}, 2, {0, 1, 0}, 0.5}, // Green sphere
-    {{-5, 0, -25}, 3, {0, 0, 1}, 0.5}, // Blue sphere
-};
-
-Light light = {{-10, 10, 10}, {1, 1, 1}};
-
-// Function prototypes for ray tracing
-int intersect_sphere(Ray ray, Sphere sphere, double *t);
-Vector trace_ray(Ray ray, int depth);
-void save_ppm(const char *filename, unsigned char *image);
-
-// Intersect a ray with a sphere
+// Sphere intersection
 int intersect_sphere(Ray ray, Sphere sphere, double *t) {
     Vector oc = vec_sub(ray.origin, sphere.center);
     double a = vec_dot(ray.direction, ray.direction);
@@ -101,107 +90,129 @@ int intersect_sphere(Ray ray, Sphere sphere, double *t) {
     double discriminant = b * b - 4 * a * c;
 
     if (discriminant < 0) return 0;
-    double sqrt_disc = sqrt(discriminant);
+    double sqrt_d = sqrt(discriminant);
 
-    double t0 = (-b - sqrt_disc) / (2.0 * a);
-    double t1 = (-b + sqrt_disc) / (2.0 * a);
-    *t = (t0 < t1 && t0 > EPSILON) ? t0 : t1;
+    double t1 = (-b - sqrt_d) / (2 * a);
+    double t2 = (-b + sqrt_d) / (2 * a);
 
+    if (t1 > EPSILON) *t = t1;
+    else if (t2 > EPSILON) *t = t2;
+    else return 0;
+
+    return 1;
+}
+
+// Plane intersection
+int intersect_plane(Ray ray, Plane plane, double *t) {
+    double denom = vec_dot(ray.direction, plane.normal);
+    if (fabs(denom) < EPSILON) return 0;
+
+    *t = vec_dot(vec_sub(plane.position, ray.origin), plane.normal) / denom;
     return *t > EPSILON;
 }
 
-// Trace a ray and compute its color
-Vector trace_ray(Ray ray, int depth) {
-    double t_min = INFINITY;
-    Sphere *hit_sphere = NULL;
+// Shadow detection
+int is_in_shadow(Vector point, Vector light_dir) {
+    Ray shadow_ray = {point, light_dir};
+    double t;
 
-    // Find the nearest intersection
+    // Check sphere shadows
     for (int i = 0; i < sizeof(spheres) / sizeof(Sphere); i++) {
-        double t;
+        if (intersect_sphere(shadow_ray, spheres[i], &t) && t > EPSILON) {
+            return 1;
+        }
+    }
+
+    // Check plane shadows
+    if (intersect_plane(shadow_ray, plane, &t) && t > EPSILON) {
+        return 1;
+    }
+
+    return 0;
+}
+
+// Ray tracing
+Vector trace_ray(Ray ray, int depth) {
+    Sphere *hit_sphere = NULL;
+    Plane *hit_plane = NULL;
+    double t_min = 1e6, t;
+    Vector color = {0, 0, 0};
+
+    // Check sphere intersections
+    for (int i = 0; i < sizeof(spheres) / sizeof(Sphere); i++) {
         if (intersect_sphere(ray, spheres[i], &t) && t < t_min) {
             t_min = t;
             hit_sphere = &spheres[i];
         }
     }
 
-    if (!hit_sphere) return (Vector){0, 0, 0}; // Background color
+    // Check plane intersection
+    if (intersect_plane(ray, plane, &t) && t < t_min) {
+        t_min = t;
+        hit_plane = &plane;
+        hit_sphere = NULL;
+    }
 
-    Vector hit_point = vec_add(ray.origin, vec_mul(ray.direction, t_min));
-    Vector normal = vec_normalize(vec_sub(hit_point, hit_sphere->center));
-    Vector light_dir = vec_normalize(vec_sub(light.position, hit_point));
+    if (hit_sphere) {
+        Vector hit_point = vec_add(ray.origin, vec_mul(ray.direction, t_min));
+        Vector normal = vec_normalize(vec_sub(hit_point, hit_sphere->center));
 
-    // Ambient lighting
-    Vector ambient = vec_mul(light.color, 0.1);
+        Vector light_dir = vec_normalize(vec_sub(light.position, hit_point));
+        Vector ambient = vec_mul(hit_sphere->color, 0.1);
 
-    // Diffuse lighting
-    double diffuse_intensity = fmax(0, vec_dot(normal, light_dir));
-    Vector diffuse = vec_mul(vec_hadamard(hit_sphere->color, light.color), diffuse_intensity);
+        Vector adjusted_point = vec_add(hit_point, vec_mul(normal, EPSILON));  // Push point slightly along normal
+        int in_shadow = is_in_shadow(adjusted_point, light_dir);
+        if (in_shadow) return ambient;
 
-    // Specular lighting
-    Vector view_dir = vec_mul(ray.direction, -1);
-    Vector reflect_dir = vec_reflect(vec_mul(light_dir, -1), normal);
-    double specular_intensity = pow(fmax(0, vec_dot(view_dir, reflect_dir)), 32);
-    Vector specular = vec_mul(light.color, specular_intensity);
+        double diffuse_intensity = fmax(0, vec_dot(normal, light_dir));
+        Vector diffuse = vec_mul(vec_hadamard(hit_sphere->color, light.color), diffuse_intensity);
 
-    // Combine lighting components
-    Vector color = vec_add(ambient, vec_add(diffuse, specular));
+        color = vec_add(ambient, diffuse);
+    } else if (hit_plane) {
+        Vector hit_point = vec_add(ray.origin, vec_mul(ray.direction, t_min));
+        Vector normal = plane.normal;
 
-    // Reflection
-    if (depth < MAX_DEPTH && hit_sphere->reflectivity > 0) {
-        Vector reflect_dir = vec_reflect(ray.direction, normal);
-        Ray reflect_ray = {vec_add(hit_point, vec_mul(normal, EPSILON)), reflect_dir};
-        Vector reflected_color = trace_ray(reflect_ray, depth + 1);
-        color = vec_add(color, vec_mul(reflected_color, hit_sphere->reflectivity));
+        Vector light_dir = vec_normalize(vec_sub(light.position, hit_point));
+        Vector ambient = vec_mul(plane.color, 0.1);
+
+        Vector adjusted_point = vec_add(hit_point, vec_mul(plane.normal, EPSILON));
+        int in_shadow = is_in_shadow(adjusted_point, light_dir);
+        if (in_shadow) return ambient;
+
+        double diffuse_intensity = fmax(0, vec_dot(normal, light_dir));
+        Vector diffuse = vec_mul(vec_hadamard(plane.color, light.color), diffuse_intensity);
+
+        color = vec_add(ambient, diffuse);
     }
 
     return color;
 }
 
-// Save the image as a PPM file
-void save_ppm(const char *filename, unsigned char *image) {
-    FILE *file = fopen(filename, "wb");
-    if (!file) {
-        perror("Failed to save image");
-        exit(1);
-    }
+// Render scene
+void render_scene(int width, int height) {
+    FILE *file = fopen("output.ppm", "w");
+    fprintf(file, "P3\n%d %d\n255\n", width, height);
 
-    fprintf(file, "P6\n%d %d\n255\n", WIDTH, HEIGHT);
-    fwrite(image, 1, WIDTH * HEIGHT * 3, file);
-    fclose(file);
-}
+    double fov = M_PI / 3.0;
 
-// Main function
-int main() {
-    unsigned char *image = malloc(WIDTH * HEIGHT * 3);
-    if (!image) {
-        perror("Failed to allocate memory for image");
-        return 1;
-    }
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            double px = (2 * (x + 0.5) / (double)width - 1) * tan(fov / 2) * width / (double)height;
+            double py = (1 - 2 * (y + 0.5) / (double)height) * tan(fov / 2);
 
-    Vector camera = {0, 0, 0};
-    double viewport_size = 1.0;
-    double aspect_ratio = (double)WIDTH / HEIGHT;
-    double focal_length = 1.0;
-
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            double px = (2 * ((x + 0.5) / WIDTH) - 1) * aspect_ratio * viewport_size;
-            double py = (1 - 2 * ((y + 0.5) / HEIGHT)) * viewport_size;
-            Vector direction = vec_normalize((Vector){px, py, -focal_length});
-            Ray ray = {camera, direction};
-
+            Ray ray = {{0, 0, 0}, vec_normalize((Vector){px, py, 1})};
             Vector color = trace_ray(ray, 0);
-            int index = (y * WIDTH + x) * 3;
-            image[index] = (unsigned char)(fmin(color.x, 1) * 255);
-            image[index + 1] = (unsigned char)(fmin(color.y, 1) * 255);
-            image[index + 2] = (unsigned char)(fmin(color.z, 1) * 255);
+
+            fprintf(file, "%d %d %d ", (int)(fmin(color.x, 1) * 255), (int)(fmin(color.y, 1) * 255), (int)(fmin(color.z, 1) * 255));
         }
     }
 
-    save_ppm("output.ppm", image);
-    free(image);
+    fclose(file);
+}
 
-    printf("Ray tracing complete! Image saved as output.ppm\n");
+int main() {
+    int width = 800, height = 600;
+    render_scene(width, height);
     return 0;
 }
 
